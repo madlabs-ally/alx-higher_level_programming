2f8f0658ca09456a483ba9f014d6b19f8bb8a169
madlabs-ally <madalitsosimbeye173@gmail.com>
2022-12-07 14:25:15 -0800

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,22 @@
 #!/usr/bin/python3
-def to_subtract(list_num):
-    to_sub = 0
-    max_list = max(list_num)
-
-    for n in list_num:
-        if max_list > n:
-            to_sub += n
-
-    return (max_list - to_sub)
 
 def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-
-    if not isinstance(roman_string, str):
-        return 0
-
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(rom_n.keys())
-
-    num = 0
-    last_rom = 0
-    list_num = [0]
-
-    for ch in roman_string:
-        for r_num in list_keys:
-            if r_num == ch:
-                if rom_n.get(ch) <= last_rom:
-                    num += to_subtract(list_num)
-                    list_num = [rom_n.get(ch)]
-                else:
-                    list_num.append(rom_n.get(ch))
-
-                last_rom = rom_n.get(ch)
-
-    num += to_subtract(list_num)
-
-    return (num)
+    """
+    converts a roman numeral character into the respective integer
+    """
+    if roman_string is None or type(roman_string) is not str:
+        return (0)
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                  'D': 500, 'M': 1000}
+    roman_list = list(roman_string.upper())
+    result = 0
+    prev = 0
+    for letter in roman_list:
+        if letter in roman_dict:
+            result += roman_dict[letter]
+            if roman_dict[letter] > prev:
+                result -= prev * 2
+            prev = roman_dict[letter]
+        else:
+            return (0)
+    return (result)
