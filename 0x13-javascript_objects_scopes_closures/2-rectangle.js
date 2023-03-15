59a9077da8037cf58c8a571ce970dee9f083e6bb
madlabs-ally <madalitsosimbeye173@gmail.com>
2023-03-14 17:23:14 -0700

    prep

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-class Rectangle {
+
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !w || !h) {
-      return undefined;
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-}
+};
