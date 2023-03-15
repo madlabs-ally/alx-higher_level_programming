f95ca0a7afafa4a4f619c4988914d860bdb05db6
madlabs-ally <madalitsosimbeye173@gmail.com>
2023-03-14 17:25:07 -0700

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
-const SquareP = require('./5-square');
 
-class Square extends SquareP {
+const Square2 = require('./5-square');
+module.exports = class Square extends Square2 {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
       }
-      console.log(s);
     }
   }
-}
-
-module.exports = Square;
+};
